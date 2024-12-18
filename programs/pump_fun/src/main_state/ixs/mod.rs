37dba7e2bdb2dc93a752fd40cabb5f1cdef21b34
AbuTuraab <aliyulekan@gmail.com>
2024-12-18 12:26:33 +0100

    initial-commit

--- programs/pump_fun/src/main_state/ixs/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod init_main_state;
-pub use init_main_state::*;
-
-pub mod transfer_ownership;
-pub use transfer_ownership::*;
-
-pub mod update_main_state;
-pub use update_main_state::*;
--- programs/pump_fun/src/pool/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod ixs;
-pub use ixs::*;
-
-pub mod state;
-pub use state::*;
-
-pub mod event;
-pub use event::*;
